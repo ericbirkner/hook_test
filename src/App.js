63b676697adcb79da621f41ca3b99ead57a0d7f6
Eric Birkner <ericbirkner@gmail.com>
2019-05-09 12:56:41 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import HeroForm from './components/HeroForm'
+import './App.css';
+
+const heroesAux = [{ id: '1', name: 'Gandalf', race: 'Maia', age: '2019', weapon: 'Staff 🏑' },
+{ id: '2', name: 'Aragorn', race: 'Human', age: '120', weapon: 'Sword ⚔' },
+{ id: '3', name: 'Legolas', race: 'Elf', age: '200', weapon: 'Bow 🏹' },
+{ id: '4', name: 'Gimli', race: 'Dwarf', age: '139', weapon: 'Axe ⚒' },
+{ id: '5', name: 'Frodo', race: 'Hobbit', age: '33', weapon: 'Dagger 🗡' }]
+
+
+//console.log(heroesAux);
+
+function App() {
+  const [heroes,setHeroes] = useState(heroesAux);
+  console.log(heroes);
+
+  const saveHero= (values)=> {
+    console.log(values);
+     setHeroes({
+        ...heroes,
+          ...values
+      });
+  }
+
+  return (
+    <div>
+      <h3> This is a Functional Component </h3>
+      <HeroForm heroSubmit={saveHero}/>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/HeroForm.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+
+function HeroForm() {
+
+  const heroObject = {
+    name: '',
+    race: '',
+    age: '',
+    weapon: ''
+  }
+
+  const [values, setValues] = useState('')
+
+  const handleChange = event => {
+    const name = event.target.name
+    const value = event.target.value
+    console.log(event.target.name);
+    setValues({
+      ...values,
+      [name]: value
+    })
+  }
+
+  const handleSubmit = event => {
+    event.preventDefault()
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" name='name' value={values.name} placeholder='name' onChange={handleChange}/>
+      <input type="text" name='race' value={values.race} placeholder='race' onChange={handleChange}/>
+      <input type="text" name='age' value={values.age} placeholder='age' onChange={handleChange}/>
+      <input type="text" name='weapon' value={values.weapon} placeholder='weapon' onChange={handleChange}/>
+      <button type='submit'>Guardar Heroe</button>
+    </form>
+  );
+};
+
+export default HeroForm
